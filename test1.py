dfea888a8aed0741454ba0ba53517652f18e9887
SM <122620728+sm2828@users.noreply.github.com>
2023-05-09 19:30:30 +0000

    static and meh

--- test1.py
+++ test1.py
@@ -7,29 +7,37 @@ run_id = uuid.uuid1()
 
 print(f'Processing run_id: {run_id}')
 
-image = Image.new('RGB', (2000, 2000))
+# Define a list of colors to choose from
+colors = [
+    (230, 220, 218),   # 
+    (185, 85, 110),   # light blue
+    (22, 22, 20),  # Red
+    (72, 68, 132),  # Red
+
+]
+
+image = Image.new('RGB', (1500, 500), (190, 201, 185))
 width, height = image.size
 
-rectangle_width = 100
+rectangle_width = 5
 rectangle_height = 100
 
-number_of_squares = random.randint(10, 550)
+number_of_squares = random.randint(1, 1000)
 
 draw_image = ImageDraw.Draw(image)
 for i in range(number_of_squares):
     rectangle_x = random.randint(0, width)
     rectangle_y = random.randint(0, height)
 
+    # Choose a random color from the list
+    color = random.choice(colors)
+
     rectangle_shape = [
         (rectangle_x, rectangle_y),
         (rectangle_x + rectangle_width, rectangle_y + rectangle_height)]
     draw_image.rectangle(
         rectangle_shape,
-        fill=(
-            random.randint(0, 255),
-            random.randint(0, 255),
-            random.randint(0, 255)
-        )
+        fill=color
     )
 
 image.save(f'{run_id}.png')
